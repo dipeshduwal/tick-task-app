0fbe903b080c599566802b02951719549810c442
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-11-14 15:30:08 +0545

    Add navigation screens to main app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './android/app/src/screens/HomeScreen';
+import TimerScreen from './android/app/src/screens/TimerScreen';
+
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <HomeScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar barStyle="dark-content" />
+        <Text style={styles.appTitle}>Tick Task App</Text>
+        <Text style={styles.quote}>“Habits are the compound interest of self-improvement.”</Text>
+
+        <Tab.Navigator
+          screenOptions={{ headerShown: false }}
+        >
+          <Tab.Screen name="To-Do Lists" component={HomeScreen} />
+          <Tab.Screen name="Timer" component={TimerScreen} />
+        </Tab.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  appTitle: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#8e44ad',
+    fontFamily: 'Cochin',
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  quote: {
+    fontSize: 14,
+    color: 'grey',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
 
 export default App;
